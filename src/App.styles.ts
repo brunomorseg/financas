bd9f2cc5c3ee62cbd7c3ca274efda3f00e41c79e
--add <brunomorseg@gmail.com>
2024-06-25 08:24:42 -0300

    Teste

---
+++ src/App.styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  
+
+`;
+
+export const Header = styled.div`
+    width: 100vw;
+    background-color: darkblue;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderH1 = styled.div`
+    padding: 0;
+    margin: 0;
+    padding-top: 30px;
+    color: white;
+    font-weight: bolder;
+    font-size: 2.2em;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
+`;
